84854a1ea45562f9d93575d54fd86982fa9b6c51
torph <73564623+Torphedo@users.noreply.github.com>
2023-11-24 01:53:53 -0500

    Apply patches It won't show me the diffs for some reason, but this should apply the patches for a centered dmenu.

--- config.h
+++ config.h
@@ -2,11 +2,9 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=15"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -26,3 +24,6 @@ static const char worddelimiters[] = " ";
 
 /* Size of the window border */
 static unsigned int border_width = 5;
+
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500; 
